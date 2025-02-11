35804d0d7fb08ef32185cd389081b34bfae156ed
Fergal Mc Carthy <fmccarthy@suse.com>
2025-02-11 16:15:15 -0500

    Updates to support SLE16 deployments
    Also update to reflect latest python versions.

--- setup.py
+++ setup.py
@@ -115,6 +115,8 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: System :: Systems Administration',
     ],
     package_dir={
